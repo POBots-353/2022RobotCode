9ee55bd782a656444e040312e6177424192c72ab
Nadav Marcus-Neuman <nmarcus-neuman@pobschools.org>
2022-01-12 14:57:32 -0500

    Changed to Motor Controller Group

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -3,8 +3,8 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -15,8 +15,8 @@ public class DriveSubsystem extends SubsystemBase {
     public CANSparkMax backLeftMotor = new CANSparkMax(Constants.leftRearMotorDeviceID, MotorType.kBrushless);
     public CANSparkMax backRightMotor = new CANSparkMax(Constants.rightRearMotorDeviceID, MotorType.kBrushless);
 
-    public SpeedControllerGroup leftMotorGroup = new SpeedControllerGroup(frontLeftMotor, backLeftMotor);
-    public SpeedControllerGroup rightMotorGroup = new SpeedControllerGroup(frontRightMotor, backRightMotor);
+    public MotorControllerGroup leftMotorGroup = new MotorControllerGroup(frontLeftMotor, backLeftMotor);
+    public MotorControllerGroup rightMotorGroup = new MotorControllerGroup(frontRightMotor, backRightMotor);
     
     DifferentialDrive differentialDrive = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
 
