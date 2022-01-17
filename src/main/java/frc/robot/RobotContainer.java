d3cb2933783fd9a247158107e479ff87f38d4b19
Jason497 <80562795+Jason497@users.noreply.github.com>
2022-01-17 04:46:17 -0500

    added a class

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,10 +9,13 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.AutoNav;
 import frc.robot.commands.TurnToAngleCommand;
+import frc.robot.commands.SetDistanceCommand;
 import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.PIDDriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 
 /**
@@ -24,7 +27,7 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final PIDDriveSubsystem driveSubsystem = new PIDDriveSubsystem();
-
+  private final SetDistanceCommand setDistance = new SetDistanceCommand();
   private final AutoNav m_autoCommand = new AutoNav(driveSubsystem);
   private final TurnToAngleCommand turnAngleCommand = new TurnToAngleCommand(driveSubsystem);
 
@@ -46,7 +49,8 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
