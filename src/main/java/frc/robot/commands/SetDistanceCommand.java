ae347ffe35f7fe6b2d011e24dad53ea6858096a1
Nadav Marcus-Neuman <nmarcus-neuman@pobschools.org>
2022-02-04 14:20:40 -0500

    Fixed Bug on Set Distance Command

--- src/main/java/frc/robot/commands/SetDistanceCommand.java
+++ src/main/java/frc/robot/commands/SetDistanceCommand.java
@@ -10,44 +10,44 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 
-
 public class SetDistanceCommand extends CommandBase {
   /** Creates a new SetDistanceCommand. */
-  //public final AnalogInput input = new AnalogInput(0);
+  // public final AnalogInput input = new AnalogInput(0);
   private final DriveSubsystem driveSubsystem;
   double neededDistance;
+
   public SetDistanceCommand(DriveSubsystem drive, double neededDistance) {
     driveSubsystem = drive;
     this.neededDistance = neededDistance;
     addRequirements(driveSubsystem);
     // Use addRequirements() here to declare subsystem dependencies.
-    
+
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-  
+  public void initialize() {
+  }
 
   @Override
   public void execute() {
-    if(Math.abs(driveSubsystem.distanceError(neededDistance)) > 3){
-      driveSubsystem.manualDrive(-driveSubsystem.distanceError(neededDistance)*0.5,0.0 , 0.0, 35);
+    if (Math.abs(driveSubsystem.distanceError(neededDistance)) > 3) {
+      driveSubsystem.manualDrive(0.0, -driveSubsystem.distanceError(neededDistance) * 0.5, 0.0, 35);
     }
     SmartDashboard.putNumber("UltraSonic", driveSubsystem.ultrasonic.getValue() * 0.125);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted){
-    //driveSubsystem.resetEncoders();
+  public void end(boolean interrupted) {
+    // driveSubsystem.resetEncoders();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(Math.abs(driveSubsystem.distanceError(neededDistance)) < 3){
-    return true;
+    if (Math.abs(driveSubsystem.distanceError(neededDistance)) < 3) {
+      return true;
     }
     return false;
   }
