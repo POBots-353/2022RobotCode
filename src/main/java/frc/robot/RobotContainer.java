2f771ea778bc14bf2f089bf67881b9635e92bc16
programming353 <58999047+programming353@users.noreply.github.com>
2022-02-05 12:29:45 -0500

    Fixed ballTransitSubsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,19 @@
 
 package frc.robot;
 
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.Buttons;
 import frc.robot.Constants.DriveConstants;
-import frc.robot.commands.AutoPositionOneCommand;
+import frc.robot.commands.TwoBallAutoCommand;
 //import frc.robot.commands.ClimberCommand;
 import frc.robot.commands.DumpBallCommand;
+import frc.robot.commands.OneBallAutoCommand;
+import frc.robot.commands.OneBallAutoCommand;
 import frc.robot.commands.SetDistanceCommand;
 import frc.robot.commands.TurnToAngleCommand;
 import frc.robot.subsystems.BallTransitSubsystem;
@@ -32,88 +38,102 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    // The robot's subsystems and commands are defined here...
-    private final DriveSubsystem driveSubsystem = new DriveSubsystem();
- 
-    // private final ClimberSubsystem climberSubsystem = new ClimberSubsystem();
-    private final BallTransitSubsystem ballTransitSubsystem = new BallTransitSubsystem();
-    // BallTransitSubsystem();
-    private final AutoPositionOneCommand autoPostion1 = new AutoPositionOneCommand(driveSubsystem, ballTransitSubsystem);
+        // The robot's subsystems and commands are defined here...
+        private final DriveSubsystem driveSubsystem = new DriveSubsystem();
 
-    public static final Joystick driverStick = new Joystick(1);
-    public static final Joystick operatorStick = new Joystick(0);
+        // private final ClimberSubsystem climberSubsystem = new ClimberSubsystem();
+        private final BallTransitSubsystem ballTransitSubsystem = new BallTransitSubsystem();
+        // BallTransitSubsystem();
+        private final TwoBallAutoCommand TwoBallAuto = new TwoBallAutoCommand(driveSubsystem, ballTransitSubsystem);
+        public final OneBallAutoCommand OneBallAuto = new OneBallAutoCommand(driveSubsystem, ballTransitSubsystem);
+        public static SendableChooser<Command> m_chooser = new SendableChooser<>();
+        // Put the chooser on the dashboard
 
-    public RobotContainer() {
-        // Configure the button bindings
-        driveSubsystem.setDefaultCommand(new RunCommand(
-                () -> driveSubsystem.manualDrive((DriveConstants.scaleX * (Math.pow(driverStick.getX(), 3))
-                        + (1 - DriveConstants.scaleY) * driverStick.getX()),
-                -(DriveConstants.scaleY * (Math.pow(driverStick.getY(), 3))
-                        + (1 - DriveConstants.scaleY) * driverStick.getY()),
-                DriveConstants.scaleTurn, DriveConstants.scaleFowd),
-                driveSubsystem));
-        configureButtonBindings();
-    }
-    
-    /*
-     * The buttons will be shecduled before the default Command and
-     * if the buttons that are activated have the same subsystem requirements as the
-     * default
-     * command,
-     * the defalut command will not run
-     */
-    /*
-     * Also, be careful about brownouts (if to many things are running at once it
-     * will cause it), especailly if your running PID on motors and neumatics, so
-     * make sure it
-     * gets disabled after use execept drive
-     */
-    private void configureButtonBindings() {
+        public static final Joystick driverStick = new Joystick(1);
+        public static final Joystick operatorStick = new Joystick(0);
 
-        new JoystickButton(driverStick, Buttons.inverseControl).whileHeld(new RunCommand(
-                () -> driveSubsystem.manualDrive(-(DriveConstants.scaleY * (Math.pow(driverStick.getY(), 3))
-                        + (1 - DriveConstants.scaleY) * driverStick.getY()),
-                        -(DriveConstants.scaleX * (Math.pow(driverStick.getX(), 3))
-                                + (1 - DriveConstants.scaleY) * driverStick.getX()),
-                        DriveConstants.scaleTurn, DriveConstants.scaleFowd),
-                driveSubsystem));
-        new JoystickButton(operatorStick, 1).whileHeld(new SetDistanceCommand(driveSubsystem,196));
+        public RobotContainer() {
+
+                m_chooser.setDefaultOption("Two Ball Auto", TwoBallAuto);
+                m_chooser.addOption("One Ball Auto", OneBallAuto);
+                SmartDashboard.putData(m_chooser);
+                // Configure the button bindings
+                driveSubsystem.setDefaultCommand(new RunCommand(
+                                () -> driveSubsystem.manualDrive(
+                                                (DriveConstants.scaleX * (Math.pow(driverStick.getX(), 3))
+                                                                + (1 - DriveConstants.scaleY) * driverStick.getX()),
+                                                -(DriveConstants.scaleY * (Math.pow(driverStick.getY(), 3))
+                                                                + (1 - DriveConstants.scaleY) * driverStick.getY()),
+                                                DriveConstants.scaleTurn, DriveConstants.scaleFowd),
+                                driveSubsystem));
+                configureButtonBindings();
+        }
+
         /*
-         * JoystickButton driveBoostButton = new JoystickButton(driverStick,
-         * Buttons.driveBoostToggle);
-         * driveBoostButton.whileHeld(new RunCommand(
-         * () -> driveSubsystem.manualDrive(-1 * (driverStick.getY()),
-         * driverStick.getX(),
-         * DriveConstants.scaleTurnBoost, DriveConstants.scaleYBoost),
-         * driveSubsystem));
-         * 
-         * new JoystickButton(driverStick, Buttons.driveSlowToggle).whileHeld(new
-         * RunCommand(
-         * () -> driveSubsystem.manualDrive(-1 * (driverStick.getY()),
-         * driverStick.getX(),
-         * DriveConstants.scaleTurnSlow, DriveConstants.scaleYSlow),
-         * driveSubsystem));
-         * */
-         // Turn to Angle Buttons
-         new JoystickButton(driverStick, Buttons.turn45Toggle).whileHeld(new TurnToAngleCommand(driveSubsystem, Constants.neededAngle45));
-         new JoystickButton(driverStick, Buttons.turn180Toggle).whileHeld(new TurnToAngleCommand(driveSubsystem, Constants.neededAngle180));
-         /*
-         * new JoystickButton(operatorStick, Buttons.climberButton)
-         * .whileHeld(new ClimberCommand(climberSubsystem));
-         * new JoystickButton(operatorStick, Buttons.dumpBallToggle).whenPressed(new
-         * DumpBallCommand(ballTransitSubsystem));
+         * The buttons will be shecduled before the default Command and
+         * if the buttons that are activated have the same subsystem requirements as the
+         * default
+         * command,
+         * the defalut command will not run
          */
-        new JoystickButton(driverStick, Buttons.turn90Toggle).whileHeld(new TurnToAngleCommand(driveSubsystem, Constants.neededAngle90));
-    }
+        /*
+         * Also, be careful about brownouts (if to many things are running at once it
+         * will cause it), especailly if your running PID on motors and neumatics, so
+         * make sure it
+         * gets disabled after use execept drive
+         */
+        private void configureButtonBindings() {
 
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        // An ExampleCommand will run in autonomous
-        return autoPostion1;
-    }
-    
+                new JoystickButton(driverStick, Buttons.inverseControl).whileHeld(new RunCommand(
+                                () -> driveSubsystem.manualDrive(
+                                                -(DriveConstants.scaleY * (Math.pow(driverStick.getY(), 3))
+                                                                + (1 - DriveConstants.scaleY) * driverStick.getY()),
+                                                -(DriveConstants.scaleX * (Math.pow(driverStick.getX(), 3))
+                                                                + (1 - DriveConstants.scaleY) * driverStick.getX()),
+                                                DriveConstants.scaleTurn, DriveConstants.scaleFowd),
+                                driveSubsystem));
+                //new JoystickButton(operatorStick, 1).whileHeld(new SetDistanceCommand(driveSubsystem, 196));
+                /*
+                 * JoystickButton driveBoostButton = new JoystickButton(driverStick,
+                 * Buttons.driveBoostToggle);
+                 * driveBoostButton.whileHeld(new RunCommand(
+                 * () -> driveSubsystem.manualDrive(-1 * (driverStick.getY()),
+                 * driverStick.getX(),
+                 * DriveConstants.scaleTurnBoost, DriveConstants.scaleYBoost),
+                 * driveSubsystem));
+                 * 
+                 * new JoystickButton(driverStick, Buttons.driveSlowToggle).whileHeld(new
+                 * RunCommand(
+                 * () -> driveSubsystem.manualDrive(-1 * (driverStick.getY()),
+                 * driverStick.getX(),
+                 * DriveConstants.scaleTurnSlow, DriveConstants.scaleYSlow),
+                 * driveSubsystem));
+                 */
+                // Turn to Angle Buttons
+                new JoystickButton(driverStick, Buttons.turn45Toggle)
+                                .whileHeld(new TurnToAngleCommand(driveSubsystem, Constants.neededAngle45));
+                new JoystickButton(driverStick, Buttons.turn180Toggle)
+                                .whileHeld(new TurnToAngleCommand(driveSubsystem, Constants.neededAngle180));
+                /*
+                 * new JoystickButton(operatorStick, Buttons.climberButton)
+                 * .whileHeld(new ClimberCommand(climberSubsystem));
+                 */
+                //Error of ending shooter motor
+                new JoystickButton(operatorStick, Buttons.dumpBallToggle)
+                                .whenPressed(new DumpBallCommand(ballTransitSubsystem));
+
+                new JoystickButton(driverStick, Buttons.turn90Toggle)
+                                .whileHeld(new TurnToAngleCommand(driveSubsystem, Constants.neededAngle90));
+        }
+
+        /**
+         * Use this to pass the autonomous command to the main {@link Robot} class.
+         *
+         * @return the command to run in autonomous
+         */
+        public Command getAutonomousCommand() {
+                return m_chooser.getSelected();
+
+        }
+
 }
