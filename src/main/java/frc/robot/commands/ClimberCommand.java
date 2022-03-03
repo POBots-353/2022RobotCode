1d39e4f95bb200c721758223171dd7a6d8def530
Nadav Marcus-Neuman <nmarcus-neuman@pobschools.org>
2022-03-03 17:56:00 -0500

    Added most of the climbing steps

--- src/main/java/frc/robot/commands/ClimberCommand.java
+++ src/main/java/frc/robot/commands/ClimberCommand.java
@@ -46,32 +46,194 @@ public class ClimberCommand extends CommandBase {
 			 * INNER AND OUTER ARMS ARE ON THE 2ND BAR BEFORE CLIMBING STARTS
 			 * Outer Position: Vertical, retracted
 			 */
+			// case 0:
+			// break;
+			// case 1:
+			// break;
+			// case 2:
+			// /*
+			// * Outer Arms: Vertical, Extended
+			// */
+			// climberSubsystem.toggleOuterArms();
+			// startTimer(Constants.timerDelayBetweenSteps);
+			// climbingStage = 3;
+			// break;
+			// case 3:
+			// /*
+			// * Outer Arms: At some position between 90 and at the bar, Extended
+			// */
+			// if (timerCompleted()) {
+			// climberSubsystem.setOuterArmsPosition(Constants.firstExtendSetPoint);
+			// startTimer(Constants.timerDelayBetweenSteps);
+			// climbingStage = 4;
+			// }
+			// break;
+			// case 4:
+			// /*
+			// Outer Arms: At some position between 90 and at the bar, Retracted */
+			// if (timerCompleted() && getPositionError(outerPosition,
+			// Constants.firstExtendSetPoint) < 1.0) {
+			// climberSubsystem.toggleOuterArms();
+			// startTimer(Constants.timerDelayBetweenSteps);
+
+			// }
+			// break;
 			case 0:
-				break;
-			case 1:
-				break;
-			case 2:
 				/*
-				 * Outer Arms: Vertical, Extended
+				 * Outer: Extended
+				 * Inner: Retracted
 				 */
 				climberSubsystem.toggleOuterArms();
 				startTimer(Constants.timerDelayBetweenSteps);
-				climbingStage = 3;
+				climbingStage = 1;
+				break;
+			case 1:
+				/*
+				 * Outer: Retracted?
+				 * Inner: Retracted
+				 */
+				if (timerCompleted()) {
+					climberSubsystem.toggleOuterArms();
+					startTimer(Constants.timerDelayBetweenSteps);
+					climbingStage = 2;
+				}
+				break;
+			case 2:
+				/*
+				 * Outer: Retracted?
+				 * Inner: Extended
+				 */
+				if (timerCompleted()) {
+					climberSubsystem.toggleInnerArms();
+					startTimer(Constants.timerDelayBetweenSteps);
+					climbingStage = 3;
+				}
 				break;
 			case 3:
 				/*
-				 * Outer Arms: At some position between 90 and at the bar, Extended
+				 * Outer: Retracted?
+				 * Inner: Retracted?
 				 */
 				if (timerCompleted()) {
-					climberSubsystem.setOuterArmsPosition(Constants.firstExtendSetPoint);
+					climberSubsystem.toggleInnerArms();
 					startTimer(Constants.timerDelayBetweenSteps);
 					climbingStage = 4;
 				}
 				break;
 			case 4:
-				if (timerCompleted() && getPositionError(outerPosition, Constants.firstExtendSetPoint) < 1.0) {
-
+				/*
+				 * Outer: Extended
+				 * Inner: Retracted
+				 */
+				if (timerCompleted()) {
+					climberSubsystem.toggleOuterArms();
+					startTimer(Constants.timerDelayBetweenSteps);
+					climbingStage = 5;
 				}
+				break;
+			case 5:
+				/*
+				 * Outer: Extended, at a position between 90 and the next bar
+				 * Inner: Retracted
+				 */
+				if (timerCompleted()) {
+					climberSubsystem.setOuterArmsPosition(Constants.firstExtendSetPoint);
+					startTimer(Constants.timerDelayBetweenSteps);
+					climbingStage = 6;
+				}
+				break;
+			case 6:
+				/*
+				 * Outer: Retracted, at a position between 90 and the next bar
+				 * Inner: Retracted
+				 */
+				if (timerCompleted() && getPositionError(outerPosition, Constants.firstExtendSetPoint) < 1.0) {
+					climberSubsystem.toggleOuterArms();
+					startTimer(Constants.timerDelayBetweenSteps);
+					climbingStage = 7;
+				}
+				break;
+			case 7:
+				/*
+				 * Outer: Retracted, behind the next bar (i think)
+				 * Inner: Retracted
+				 */
+				if (timerCompleted()) {
+					climberSubsystem.setOuterArmsPosition(Constants.behindBarSetPoint);
+					startTimer(Constants.timerDelayBetweenSteps);
+					climbingStage = 8;
+				}
+				break;
+			case 8:
+				/*
+				 * Outer: Extended, behind the next bar (i think)
+				 * Inner: Retracted
+				 */
+				if (timerCompleted() && getPositionError(outerPosition, Constants.behindBarSetPoint) < 1.0) {
+					climberSubsystem.toggleOuterArms();
+					startTimer(Constants.timerDelayBetweenSteps);
+					climbingStage = 9;
+				}
+				break;
+			case 9:
+				/*
+				 * Outer: Extended, aligned with the next bar
+				 * Inner: Retracted
+				 */
+				if (timerCompleted()) {
+					climberSubsystem.setOuterArmsPosition(Constants.barAlignedSetPoint);
+					startTimer(Constants.timerDelayBetweenSteps);
+					climbingStage = 10;
+				}
+				break;
+			case 10:
+				/*
+				 * Outer: Retracted, Aligned with the next bar
+				 * Inner: Retracted
+				 */
+				if (timerCompleted() && getPositionError(outerPosition, Constants.barAlignedSetPoint) < 1.0) {
+					climberSubsystem.toggleOuterArms();
+					startTimer(Constants.MLGWaterBucketClutchTime);
+					climbingStage = 11;
+				}
+				break;
+			case 11:
+				/*
+				 * We Disable PID at the perfect time to prevent the compressor from exploding
+				 * or the motors/arms breaking
+				 */
+				if (timerCompleted()) {
+					climberSubsystem.setOuterPID(false);
+					startTimer(Constants.timerDelayBetweenSteps); // I might not want this but I'm keeping it here temporarily
+					climbingStage = 12;
+				}
+				break;
+			case 12:
+				/*
+				 * Outer: Retracted, Unknown Angle
+				 * Inner: Retracted
+				 */
+				if (timerCompleted()) {
+					climberSubsystem.setOuterArmsPosition(outerPosition);
+					climberSubsystem.setOuterPID(true);
+					startTimer(Constants.timerDelayBetweenSteps);
+					climbingStage = 13;
+				}
+				break;
+			case 13:
+				/*
+				 * Outer: Retracted, Unknown Angle
+				 * Inner: Extended
+				 */
+				if (timerCompleted()) {
+					climberSubsystem.toggleInnerArms();
+					climbingStage = 14;
+				}
+				break;
+			case 14:
+				/*
+				 * I have no idea
+				 */
 				break;
 		}
 	}
@@ -80,7 +242,6 @@ public class ClimberCommand extends CommandBase {
 	@Override
 	public void end(boolean interrupted) {
 		climberSubsystem.outerMotor.set(0);
-		climberSubsystem.leftInnerMotor.set(0);
 	}
 
 	// Returns true when the command should end.
